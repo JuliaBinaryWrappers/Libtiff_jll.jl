4b9d164d694b4c36334edcd1e21da64107acd641
jlbuild <juliabuildbot@gmail.com>
2020-04-09 10:58:10 +0000

    Libtiff_jll build 4.0.10+1

--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -27,14 +27,18 @@ const libtiff = "libtiff.so.5"
 Open all libraries
 """
 function __init__()
-    global prefix = abspath(joinpath(@__DIR__, ".."))
+    global artifact_dir = abspath(artifact"Libtiff")
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    append!.(Ref(PATH_list), (JpegTurbo_jll.PATH_list, Zlib_jll.PATH_list, Zstd_jll.PATH_list,))
-    append!.(Ref(LIBPATH_list), (JpegTurbo_jll.LIBPATH_list, Zlib_jll.LIBPATH_list, Zstd_jll.LIBPATH_list,))
+    # We first need to add to LIBPATH_list the libraries provided by Julia
+    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
+    # then append them to our own.
+    foreach(p -> append!(PATH_list, p), (JpegTurbo_jll.PATH_list, Zlib_jll.PATH_list, Zstd_jll.PATH_list,))
+    foreach(p -> append!(LIBPATH_list, p), (JpegTurbo_jll.LIBPATH_list, Zlib_jll.LIBPATH_list, Zstd_jll.LIBPATH_list,))
 
-    global libtiff_path = abspath(joinpath(artifact"Libtiff", libtiff_splitpath...))
+    global libtiff_path = normpath(joinpath(artifact_dir, libtiff_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.
