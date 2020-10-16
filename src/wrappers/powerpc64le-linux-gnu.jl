b32efbcf06f221c80e78e622087d9f25dd78e0b2
jlbuild <juliabuildbot@gmail.com>
2020-10-16 18:59:10 +0000

    Libtiff_jll build 4.1.0+2

--- src/wrappers/powerpc64le-linux-gnu.jl
+++ src/wrappers/powerpc64le-linux-gnu.jl
@@ -4,52 +4,15 @@ export libtiff
 using JpegTurbo_jll
 using Zlib_jll
 using Zstd_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libtiff`
-const libtiff_splitpath = ["lib", "libtiff.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libtiff_path = ""
-
-# libtiff-specific global declaration
-# This will be filled out by __init__()
-libtiff_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libtiff = "libtiff.so.5"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Libtiff")
+JLLWrappers.@declare_library_product(libtiff, "libtiff.so.5")
 function __init__()
-    global artifact_dir = abspath(artifact"Libtiff")
+    JLLWrappers.@generate_init_header(JpegTurbo_jll, Zlib_jll, Zstd_jll)
+    JLLWrappers.@init_library_product(
+        libtiff,
+        "lib/libtiff.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (JpegTurbo_jll.PATH_list, Zlib_jll.PATH_list, Zstd_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (JpegTurbo_jll.LIBPATH_list, Zlib_jll.LIBPATH_list, Zstd_jll.LIBPATH_list,))
-
-    global libtiff_path = normpath(joinpath(artifact_dir, libtiff_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libtiff_handle = dlopen(libtiff_path)
-    push!(LIBPATH_list, dirname(libtiff_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
