fddff204e013fe40cff4fac05fa9289adaaa8034
jlbuild <juliabuildbot@gmail.com>
2023-06-22 14:15:42 +0000

    Libtiff_jll build 4.5.1+0

--- src/wrappers/i686-linux-gnu.jl
+++ src/wrappers/i686-linux-gnu.jl
@@ -3,15 +3,17 @@ export libtiff
 
 using JpegTurbo_jll
 using LERC_jll
+using XZ_jll
 using Zlib_jll
 using Zstd_jll
+using libwebp_jll
 JLLWrappers.@generate_wrapper_header("Libtiff")
-JLLWrappers.@declare_library_product(libtiff, "libtiff.so.5")
+JLLWrappers.@declare_library_product(libtiff, "libtiff.so.6")
 function __init__()
-    JLLWrappers.@generate_init_header(JpegTurbo_jll, LERC_jll, Zlib_jll, Zstd_jll)
+    JLLWrappers.@generate_init_header(JpegTurbo_jll, LERC_jll, XZ_jll, Zlib_jll, Zstd_jll, libwebp_jll)
     JLLWrappers.@init_library_product(
         libtiff,
-        "lib/libtiff.so",
+        "lib/libtiff.so.6",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -3,15 +3,17 @@ export libtiff
 
 using JpegTurbo_jll
 using LERC_jll
+using XZ_jll
 using Zlib_jll
 using Zstd_jll
+using libwebp_jll
 JLLWrappers.@generate_wrapper_header("Libtiff")
-JLLWrappers.@declare_library_product(libtiff, "libtiff-5.dll")
+JLLWrappers.@declare_library_product(libtiff, "libtiff-6.dll")
 function __init__()
-    JLLWrappers.@generate_init_header(JpegTurbo_jll, LERC_jll, Zlib_jll, Zstd_jll)
+    JLLWrappers.@generate_init_header(JpegTurbo_jll, LERC_jll, XZ_jll, Zlib_jll, Zstd_jll, libwebp_jll)
     JLLWrappers.@init_library_product(
         libtiff,
-        "bin\\libtiff-5.dll",
+        "bin\\libtiff-6.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
