574e4d33250a3641c83eb63b5d96816df03cd92c
jlbuild <juliabuildbot@gmail.com>
2023-06-22 15:57:28 +0000

    Libtiff_jll build 4.5.1+1

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -6,11 +6,10 @@ using LERC_jll
 using XZ_jll
 using Zlib_jll
 using Zstd_jll
-using libwebp_jll
 JLLWrappers.@generate_wrapper_header("Libtiff")
 JLLWrappers.@declare_library_product(libtiff, "@rpath/libtiff.6.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(JpegTurbo_jll, LERC_jll, XZ_jll, Zlib_jll, Zstd_jll, libwebp_jll)
+    JLLWrappers.@generate_init_header(JpegTurbo_jll, LERC_jll, XZ_jll, Zlib_jll, Zstd_jll)
     JLLWrappers.@init_library_product(
         libtiff,
         "lib/libtiff.6.dylib",
--- src/wrappers/x86_64-unknown-freebsd.jl
+++ src/wrappers/x86_64-unknown-freebsd.jl
@@ -6,14 +6,13 @@ using LERC_jll
 using XZ_jll
 using Zlib_jll
 using Zstd_jll
-using libwebp_jll
 JLLWrappers.@generate_wrapper_header("Libtiff")
 JLLWrappers.@declare_library_product(libtiff, "libtiff.so.6")
 function __init__()
-    JLLWrappers.@generate_init_header(JpegTurbo_jll, LERC_jll, XZ_jll, Zlib_jll, Zstd_jll, libwebp_jll)
+    JLLWrappers.@generate_init_header(JpegTurbo_jll, LERC_jll, XZ_jll, Zlib_jll, Zstd_jll)
     JLLWrappers.@init_library_product(
         libtiff,
-        "lib/libtiff.so.6",
+        "lib/libtiff.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
